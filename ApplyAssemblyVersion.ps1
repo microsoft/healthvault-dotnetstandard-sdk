50d7db432bdeb3cd1ba98f560db9a8e4530607ff
David Woods <dwoo@microsoft.com>
2017-02-21 16:40:00 -0800

    Adding files to support the OneSDK build definition

---
+++ ApplyAssemblyVersion.ps1
@@ -0,0 +1,41 @@
+<#
+  <copyright file="ApplyAssemblyVersion.ps1" company="Microsoft Corporation">
+   Copyright (C) Microsoft Corporation.  All rights reserved.
+  </copyright>
+  <summary>
+    This script prepares the SharedVersionInfo.cs so that it contains the right build info to stamp into the built files.
+  </summary>
+ #>
+
+param
+(
+   [Parameter(Mandatory=$true)]
+   [string]$BuildNumber,
+
+   [Parameter(Mandatory=$true)]
+   [string]$SourcesPath
+)
+
+$parts = $BuildNumber.Split('.');
+
+$sprint = 0;
+
+if ($parts[1] -match '^s[0-9]+$')
+{
+    $sprint = $parts[1].TrimStart('s');
+}
+
+$build = "" + ($parts[2].Substring(0,4) % 5) + $parts[2].Substring(4);
+$revision = $parts[3];
+
+$AssemblyVersion = "1.$sprint.0.0"
+$AssemblyFileVersion = "1.$sprint.$build.$revision"
+Write-Host "##vso[task.setvariable variable=AssemblyFileVersion;]$AssemblyFileVersion";
+
+$file = Join-Path -Path $SourcesPath -ChildPath 'SharedVersionInfo.cs'
+Set-ItemProperty $file -Name IsReadOnly -Value $false
+
+$content = Get-Content $file
+$content = $content -replace "1.0.0.0", $AssemblyVersion
+$content = $content -replace "1.0.1.1", $AssemblyFileVersion
+Set-Content -Encoding Default -Path $file -Value $content
