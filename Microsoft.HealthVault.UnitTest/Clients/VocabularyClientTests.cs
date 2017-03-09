5d547cee7a0d37838739e47b3c13f44baafd00bd
William Shelor <wishel@microsoft.com>
2017-03-09 15:06:29 -0800

    update to use new mocking framework

--- Microsoft.HealthVault.UnitTest/Clients/VocabularyClientTests.cs
+++ Microsoft.HealthVault.UnitTest/Clients/VocabularyClientTests.cs
@@ -1,16 +1,29 @@
-﻿using Microsoft.HealthVault.Clients;
+﻿using System.Threading.Tasks;
+using Microsoft.HealthVault.Clients;
+using Microsoft.HealthVault.Connection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 
 namespace Microsoft.HealthVault.UnitTest.Clients
 {
     [TestClass]
     public class VocabularyClientTests
     {
+
         [TestMethod]
         public void CreateClient()
         {
-            VocabularyClient client = new VocabularyClient();
+            var connection = Substitute.For<IConnectionInternal>();
+            VocabularyClient client = new VocabularyClient { Connection = connection };
             Assert.IsTrue(client.Connection != null);
+        }
+
+        [TestMethod]
+        public async Task GetVocabulary()
+        {
+            VocabularyClient client = new VocabularyClient();
+            await client.GetVocabularyKeysAsync();
+
         }
     }
 }
