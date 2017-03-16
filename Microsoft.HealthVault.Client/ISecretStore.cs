34c4e0ac246e26a65ba03b498069728a2b8efbd3
William Shelor <wishel@microsoft.com>
2017-03-16 01:38:30 +0000

    Merged PR 6599: Interface updates
    Interface updates

---
+++ Microsoft.HealthVault.Client/ISecretStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.HealthVault.Client
+{
+    internal interface ISecretStore
+    {
+        Task WriteAsync(string key, string contents);
+
+        Task<string> ReadAsync(string key);
+
+        Task DeleteAsync(string key);
+    }
+}
--- Microsoft.HealthVault.Client/LocalObjectStore.cs
+++ Microsoft.HealthVault.Client/LocalObjectStore.cs
@@ -8,38 +8,28 @@ namespace Microsoft.HealthVault.Client
 {
     internal class LocalObjectStore : ILocalObjectStore
     {
-        private readonly IFileStore fileStore;
+        private readonly ISecretStore secretStore;
         private readonly ILocalDataProtection dataProtection;
 
-        public LocalObjectStore(IFileStore fileStore, ILocalDataProtection dataProtection)
+        public LocalObjectStore(ISecretStore secretStore, ILocalDataProtection dataProtection)
         {
-            this.fileStore = fileStore;
+            this.secretStore = secretStore;
             this.dataProtection = dataProtection;
         }
 
-        public async Task<T> ReadObjectAsync<T>(string key)
+        public async Task<T> ReadAsync<T>(string key)
         {
             return default(T);
         }
 
-        public async Task WriteObjectAsync<T>(string key, T value)
+        public async Task WriteAsync(string key, object value)
         {
             
         }
 
-        public async Task ClearObjectAsync(string key)
+        public async Task DeleteAsync(string key)
         {
             
-        }
-
-        public async Task<T> ReadEncryptedObjectAsync<T>(string key)
-        {
-            return default(T);
-        }
-
-        public async Task WriteEncryptedObjectAsync<T>(string key, T value)
-        {
-
         }
     }
 }
