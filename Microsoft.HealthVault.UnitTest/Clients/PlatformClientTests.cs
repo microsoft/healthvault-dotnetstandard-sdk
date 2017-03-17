eb9e85933e1e759cea2887e09979ff3d161f7339
hatims <hatims@microsoft.com>
2017-03-17 15:25:51 -0700

    Added sample response to platform client to test selectinstance method

--- Microsoft.HealthVault.UnitTest/Clients/PlatformClientTests.cs
+++ Microsoft.HealthVault.UnitTest/Clients/PlatformClientTests.cs
@@ -33,13 +33,12 @@ namespace Microsoft.HealthVault.UnitTest.Clients
         [TestMethod]
         public async Task SelectInstanceTest()
         {
-            var location = new Location("US", "Washington");
+            var location = new Location("US", "WA");
 
             var response = new HealthServiceResponseData
             {
-                //TODO: We need to add the sample response data once we are a ble to produce it
-                //InfoNavigator = new XPathDocument(new StringReader(SampleUtils.GetSampleContent("InstanceSample.xml"))).CreateNavigator(),
-                //ResponseText = new ArraySegment<byte>(Encoding.ASCII.GetBytes(SampleUtils.GetSampleContent("InstanceSample.xml")))
+                InfoNavigator = new XPathDocument(new StringReader(SampleUtils.GetSampleContent("InstanceSample.xml"))).CreateNavigator(),
+                ResponseText = new ArraySegment<byte>(Encoding.ASCII.GetBytes(SampleUtils.GetSampleContent("InstanceSample.xml")))
             };
 
             connection.ExecuteAsync(HealthVaultMethods.SelectInstance, Arg.Any<int>(), Arg.Any<string>()).Returns(response);
@@ -47,6 +46,32 @@ namespace Microsoft.HealthVault.UnitTest.Clients
             var result = await platformClient.SelectInstanceAsync(location).ConfigureAwait(false);
 
             await this.connection.Received().ExecuteAsync(HealthVaultMethods.SelectInstance, Arg.Any<int>(), Arg.Any<string>());
+
+        }
+
+        [TestMethod]
+        public async Task SelectInstanceLocationNullArgumrentTest()
+        {
+            Location location = null;
+
+            var response = new HealthServiceResponseData
+            {
+                InfoNavigator = new XPathDocument(new StringReader(SampleUtils.GetSampleContent("InstanceSample.xml"))).CreateNavigator(),
+                ResponseText = new ArraySegment<byte>(Encoding.ASCII.GetBytes(SampleUtils.GetSampleContent("InstanceSample.xml")))
+            };
+
+            connection.ExecuteAsync(HealthVaultMethods.SelectInstance, Arg.Any<int>(), Arg.Any<string>()).Returns(response);
+            try
+            {
+                var result = await platformClient.SelectInstanceAsync(location).ConfigureAwait(false);
+
+                await this.connection.Received()
+                    .ExecuteAsync(HealthVaultMethods.SelectInstance, Arg.Any<int>(), Arg.Any<string>());
+            }
+            catch (ArgumentNullException e)
+            {
+                Assert.IsTrue(e != null);
+            }
 
         }
 
@@ -78,7 +103,7 @@ namespace Microsoft.HealthVault.UnitTest.Clients
         [TestMethod]
         public async Task GetServiceDefinitionWithDateTimeTest()
         {
-            DateTime lastUpdatedTime = DateTime.Now;
+            DateTime lastUpdatedTime = new DateTime(2017, 03, 17, 03, 24, 19);
             var response = GetDefinition();
 
             connection.ExecuteAsync(HealthVaultMethods.GetServiceDefinition, Arg.Any<int>(), Arg.Any<string>()).Returns(response);
@@ -86,13 +111,13 @@ namespace Microsoft.HealthVault.UnitTest.Clients
             var result = await platformClient.GetServiceDefinitionAsync(lastUpdatedTime).ConfigureAwait(false);
             await this.connection.Received().ExecuteAsync(HealthVaultMethods.GetServiceDefinition, Arg.Any<int>(), Arg.Any<string>());
 
-            Assert.AreEqual(result.LastUpdated.ToString(), "3/17/2017 3:24:19 AM");
+            Assert.AreEqual(result.LastUpdated, lastUpdatedTime);
         }
 
         [TestMethod]
         public async Task GetServiceDefinitionWithSectionsDateTimeTest()
         {
-            DateTime lastUpdatedTime = DateTime.Now;
+            DateTime lastUpdatedTime = new DateTime(2017, 03, 17, 03, 24, 19);
             var response = GetDefinition();
 
             connection.ExecuteAsync(HealthVaultMethods.GetServiceDefinition, Arg.Any<int>(), Arg.Any<string>()).Returns(response);
@@ -100,7 +125,7 @@ namespace Microsoft.HealthVault.UnitTest.Clients
             var result = await platformClient.GetServiceDefinitionAsync(ServiceInfoSections.All, lastUpdatedTime).ConfigureAwait(false);
             await this.connection.Received().ExecuteAsync(HealthVaultMethods.GetServiceDefinition, Arg.Any<int>(), Arg.Any<string>());
 
-            Assert.AreEqual(result.LastUpdated.ToString(), "3/17/2017 3:24:19 AM");
+            Assert.AreEqual(result.LastUpdated, lastUpdatedTime);
         }
 
         private HealthServiceResponseData GetDefinition()
