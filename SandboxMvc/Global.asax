912f1a3a058840646300b82d73950b694a1c27b7
David Rickard (USA) <davricka@microsoft.com>
2017-03-23 22:18:59 +0000

    Merged PR 6744: Multi-record fix and Sample -> Sandbox rename
    Changed to save all records on client so multi-record apps can work. Also updated UWP sandbox app to use selected record as an SRA.
    
    Also renamed all apps "Sandbox___" to help differentiate them from the public sample apps we will create.

---
+++ SandboxMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SandboxMvc.MvcApplication" Language="C#" %>
---
+++ SandboxUwp/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.HealthVault.Client;
+using Microsoft.HealthVault.Clients;
+using Microsoft.HealthVault.ItemTypes;
+using Microsoft.HealthVault.Record;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SandboxUwp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private IClientHealthVaultConnection connection;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Connect_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.OutputBlock.Text = "Connecting...";
+
+            var configuration = new ClientHealthVaultConfiguration
+            {
+                MasterApplicationId = Guid.Parse("d6318dff-5352-4a10-a140-6c82c6536a3b")
+            };
+
+            ClientHealthVaultFactory.Current.SetConfiguration(configuration);
+            this.connection = await ClientHealthVaultFactory.Current.GetConnectionAsync();
+
+            this.OutputBlock.Text = "Connected.";
+        }
+
+        private async void Get_BP_OnClick(object sender, RoutedEventArgs e)
+        {
+            HealthRecordInfo recordInfo = this.connection.PersonInfo.SelectedRecord;
+            IThingClient thingClient = this.connection.GetThingClient();
+
+            var bloodPressures = await thingClient.GetThingsAsync<BloodPressure>(recordInfo);
+            BloodPressure firstBloodPressure = bloodPressures.FirstOrDefault();
+            if (firstBloodPressure == null)
+            {
+                this.OutputBlock.Text = "No blood pressures.";
+            }
+            else
+            {
+                this.OutputBlock.Text = firstBloodPressure.Systolic + "/" + firstBloodPressure.Diastolic;
+            }
+        }
+
+        private async void SetBP_OnClick(object sender, RoutedEventArgs e)
+        {
+            HealthRecordInfo recordInfo = this.connection.PersonInfo.SelectedRecord;
+            IThingClient thingClient = this.connection.GetThingClient();
+
+            await thingClient.CreateNewThingsAsync(recordInfo, new List<BloodPressure> { new BloodPressure(new HealthServiceDateTime(DateTime.Now), 117, 70) });
+
+            this.OutputBlock.Text = "Created blood pressure.";
+        }
+    }
+}
