f488edf1ce8030f29701ccd46bed0bb169b3e4b3
Brendan Kowitz <bkowitz@microsoft.com>
2017-04-19 12:25:10 -0700

    #54575 Naming and structure changes

---
+++ Microsoft.HealthVault.RestApi/HealthVaultRestCredentials.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.HealthVault.Clients;
+using Microsoft.Rest;
+
+namespace Microsoft.HealthVault.RestApi
+{
+    public class HealthVaultRestCredentials : ServiceClientCredentials
+    {
+        private readonly IHealthVaultRestClient client;
+        private readonly Guid recordId;
+
+        public HealthVaultRestCredentials(IHealthVaultRestClient client, Guid recordId)
+        {
+            this.client = client;
+            this.recordId = recordId;
+        }
+
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            this.client.AuthorizeRestRequest(request, this.recordId);
+            return Task.FromResult(true);
+        }
+    }
+}
