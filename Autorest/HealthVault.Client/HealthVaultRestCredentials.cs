7d6d4dd53d8d5079ad8348bd273a888b02041e45
Brendan Kowitz <bkowitz@microsoft.com>
2017-04-19 09:14:51 -0700

    Adds autorest generated client

---
+++ Autorest/HealthVault.Client/HealthVaultRestCredentials.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.HealthVault.Clients;
+using Microsoft.Rest;
+
+namespace HealthVault.Client
+{
+    public class HealthVaultRestCredentials : ServiceClientCredentials
+    {
+        private readonly IHealthVaultRestClient client;
+        private readonly Guid recordId;
+
+        public HealthVaultRestCredentials(IHealthVaultRestClient client, Guid recordId)
+        {
+            this.client = client;
+            this.recordId = recordId;
+        }
+
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            this.client.AuthorizeRestRequest(request, this.recordId);
+            return Task.FromResult(true);
+        }
+    }
+}
