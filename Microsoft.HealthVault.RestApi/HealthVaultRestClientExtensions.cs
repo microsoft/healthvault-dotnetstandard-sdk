818079587974418ba375440d4f769fb456b67e5c
Brendan Kowitz <brendan.kowitz@gmail.com>
2017-04-20 09:01:31 -0700

    #54575 Adds autorest generated client (#7)

---
+++ Microsoft.HealthVault.RestApi/HealthVaultRestClientExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using Microsoft.HealthVault.Connection;
+using Microsoft.HealthVault.RestApi.Generated;
+
+namespace Microsoft.HealthVault.RestApi
+{
+    /// <summary>
+    /// Provides extension methods for creating <see cref="IMicrosoftHealthVaultRestApi"/>
+    /// </summary>
+    public static class HealthVaultRestClientExtensions
+    {
+        /// <summary>
+        /// Creates a client for accessing the Microsoft HealthVault REST API.
+        /// </summary>
+        /// <param name="connection">The client.</param>
+        /// <param name="restServiceBaseUrl">The rest service base URL.</param>
+        /// <param name="recordId">The record identifier.</param>
+        /// <returns></returns>
+        public static IMicrosoftHealthVaultRestApi CreateMicrosoftHealthVaultRestApi(this IHealthVaultConnection connection, Uri restServiceBaseUrl, Guid recordId)
+        {
+            return new MicrosoftHealthVaultRestApi(restServiceBaseUrl, new HealthVaultRestCredentials(connection.CreateRestClient(), recordId));
+        }
+    }
+}
---
+++ Microsoft.HealthVault.RestApi/Readme.md
@@ -0,0 +1,14 @@
+﻿# HealthVault Client
+
+This client is generated using AutoRest from the HealthVault [Swagger definition](https://developer.healthvault.com/Api).
+
+## AutoRest
+
+The AutoRest tool can be installed by following the instructions at the [AutoRest page](https://github.com/Azure/autorest).
+
+## Regenerating
+An example of the command to regenerate this client is as follows:
+
+```
+AutoRest -Namespace HealthVault.Client -CodeGenerator CSharp -Modeler Swagger -Input c:\temp\swagger.json -PackageName HealthVault.Client -AddCredentials true
+```
