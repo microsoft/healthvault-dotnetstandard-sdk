495aa9594a438d99b34af39a38f2a3aaa01df47d
David Woods <dwoo@microsoft.com>
2017-05-23 16:59:14 -0700

    Fixing bad access modifier
    - Tool chose the wrong one

--- SandboxXamarinForms/SandboxXamarinForms.UWP/App.xaml.cs
+++ SandboxXamarinForms/SandboxXamarinForms.UWP/App.xaml.cs
@@ -10,7 +10,7 @@ namespace SandboxXamarinForms.UWP
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    internal sealed partial class App : Application
+    public sealed partial class App : Application
     {
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
@@ -65,6 +65,7 @@ namespace SandboxXamarinForms.UWP
                 // parameter
                 rootFrame.Navigate(typeof(MainPage), e.Arguments);
             }
+
             // Ensure the current window is active
             Window.Current.Activate();
         }
@@ -89,6 +90,7 @@ namespace SandboxXamarinForms.UWP
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
+
             //TODO: Save application state and stop any background activity
             deferral.Complete();
         }
