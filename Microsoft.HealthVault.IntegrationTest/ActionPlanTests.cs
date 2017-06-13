f38c892e7b7143546665b91b11a9cd6cdf8ac937
Brendan Kowitz <bkowitz@microsoft.com>
2017-06-13 13:33:02 -0700

    Updates schedules object in test
    (cherry picked from commit 5a6bef7047a6587466c3b4d662f705a71643c75b)
    
    # Conflicts:
    #	Microsoft.HealthVault.IntegrationTest/ActionPlanTests.cs

--- Microsoft.HealthVault.IntegrationTest/ActionPlanTests.cs
+++ Microsoft.HealthVault.IntegrationTest/ActionPlanTests.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Microsoft.HealthVault.Client;
@@ -30,8 +39,8 @@ namespace Microsoft.HealthVault.IntegrationTest
 
             await RemoveAllActionPlansAsync(restClient);
 
-            Guid objectiveId = Guid.NewGuid();
-            await restClient.ActionPlans.CreateAsync(CreateWeightActionPlan(objectiveId));
+            Guid planId = Guid.NewGuid();
+            await restClient.ActionPlans.CreateAsync(CreateWeightActionPlan(planId));
 
             var plans = await restClient.ActionPlans.GetAsync();
             Assert.AreEqual(1, plans.Plans.Count);
@@ -50,7 +59,7 @@ namespace Microsoft.HealthVault.IntegrationTest
             }
         }
 
-        private static ActionPlanV2 CreateWeightActionPlan(Guid objectiveId)
+        private static ActionPlanV2 CreateWeightActionPlan(Guid planId)
         {
             var plan = new ActionPlanV2();
             var objective = new Objective
@@ -111,6 +120,14 @@ namespace Microsoft.HealthVault.IntegrationTest
                     }
                 },
                 CompletionType = "Frequency",
+                Schedules = new List<ScheduleV2>
+                {
+                    new ScheduleV2
+                    {
+                        ReminderState = "Off",
+                        ScheduledDays = new Collection<string> { "Everyday" }
+                    }  
+                },
                 FrequencyTaskCompletionMetrics = new ActionPlanFrequencyTaskCompletionMetricsV2()
                 {
                     OccurrenceCount = 1,
