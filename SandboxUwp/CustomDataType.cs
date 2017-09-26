a6cb2b49fabcc5318f6fbea91bdd7442e9444d80
Surya Renduchintala <suryar@microsoft.com>
2017-09-26 11:15:22 -0700

    Example to add custom data type via Connection factory in UWP sandbox app

---
+++ SandboxUwp/CustomDataType.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using Microsoft.HealthVault.ItemTypes;
+
+namespace SandboxUwp
+{
+    class CustomDataType : ApplicationSpecific
+    {
+        // Add custom properties here
+        public long StartTime { get; set; }
+        public long EndTime { get; set; }
+        public List<double> Values { get; set; }
+        // Custom properties end
+
+        private HealthServiceDateTime m_when;
+
+        // HealthVault expects a specific data schema - custom properties are added at the end
+        public override void WriteXml(XmlWriter writer)
+        {
+            m_when = new HealthServiceDateTime();
+
+            writer.WriteStartElement("app-specific");
+            {
+                writer.WriteStartElement("format-appid");
+                writer.WriteValue("custom-data-type");
+                writer.WriteEndElement();
+
+                writer.WriteStartElement("format-tag");
+                writer.WriteValue("custom-data-type");
+                writer.WriteEndElement();
+
+                m_when.WriteXml("when", writer);
+
+                writer.WriteStartElement("summary");
+                writer.WriteValue("");
+                writer.WriteEndElement();
+
+                // Custom properties start
+                writer.WriteStartElement("CustomDataType");
+                writer.WriteAttributeString("StartTime", StartTime.ToString());
+                writer.WriteAttributeString("EndTime", EndTime.ToString());
+                writer.WriteAttributeString("Values", string.Join(",", Values));
+                writer.WriteEndElement();
+
+                // Custom properties end
+            }
+            writer.WriteEndElement();
+        }
+    }
+}
